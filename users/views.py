adc5269ce8a6b41de8b1cbb7b00e36881aa5535e
Dilyara <dilnov2008@mail.ru>
2024-01-27 14:54:37 +0300

    DRF Homework 4

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+from django.contrib.auth.hashers import make_password
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        user = super().perform_create(serializer)
+
+        if user is not None:
+            password = self.request.data['password']
+            hashed_password = make_password(password)
+            user.set_password(hashed_password)
+            user.save()
+
+        return user
